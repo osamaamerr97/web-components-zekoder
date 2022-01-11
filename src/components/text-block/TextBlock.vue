8ed6f2070694136e42e69f57f606b25087a7885f
AwaisAshraf <sh.awais14@gmail.com>
2022-01-11 14:06:23 +0500

    minor fixes in text block and heading block

--- src/components/text-block/TextBlock.vue
+++ src/components/text-block/TextBlock.vue
@@ -14,19 +14,14 @@
     props: ['text', 'spaceBefore', 'spaceAfter', 'lineBreaks', 'styleObj'],
     data() {
         return {
-            styleObject : {
-                
-            }
+            styleObject : {}
         }
-    },
-    methods: {
-        
     },
     created() {
         let marginB = this.spaceAfter || 10;
         marginB += this.lineBreaks? this.lineBreaks*10 : 0;
         this.styleObject = { ... this.styleObj,
-            marginTop: this.spaceBefore || '10px',
+            marginTop: this.spaceBefore+'px' || '10px',
             marginBottom: marginB + 'px'
         }
     }

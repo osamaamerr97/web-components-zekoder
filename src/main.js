eeeaff418e6de6b83e0078da6b80620b75ac16a1
AwaisAshraf <sh.awais14@gmail.com>
2022-01-21 14:31:10 +0500

    Slider component implemented. Will be updated later once the card component is implemented

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import ActionButton from './components/action-button/ActionButton.vue';
 import HeadingBlock from './components/heading-block/HeadingBlock.vue';
 import HtmlBlock from './components/html-block/HtmlBlock.vue';
 import ListsBlock from './components/lists-block/ListsBlock.vue';
+import Slider from './components/slider/Slider.vue';
 import TextBlock from './components/text-block/TextBlock.vue';
-export {ActionButton,HeadingBlock,HtmlBlock,ListsBlock,TextBlock}
+
+export {ActionButton,HeadingBlock,HtmlBlock,ListsBlock,Slider,TextBlock}

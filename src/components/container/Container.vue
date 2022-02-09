b32866e087ae6013aef64784abb0766cd09b67f6
AbdAsh <59181667+AbdAsh@users.noreply.github.com>
2022-02-09 17:45:36 +0300

    Initial Container Element

---
+++ src/components/container/Container.vue
@@ -0,0 +1,57 @@
+<template>
+  <div :style="styleObject">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ZekContainer",
+  props: {
+    direction: {
+      type: String,
+    },
+    backgroundColor: {
+      type: String,
+    },
+    padding: {
+      type: String,
+    },
+    width: {
+      type: Number,
+    },
+    height: {
+      type: String,
+    },
+    size: {
+      type: String,
+      default: "flexible",
+    },
+    styleObj: {
+      type: Object,
+    },
+  },
+  created() {
+    this.styleObject = {
+      ...this.styleObj,
+    };
+  },
+  computed: {
+    styleObject() {
+      return {
+        ...this.styleObj,
+        display: "flex",
+        overflow: "auto",
+        "flex-direction": this.direction || "",
+        "background-color": this.backgroundColor || "",
+        padding: this.padding || "",
+        width:
+          this.size == "absolute"
+            ? `${(this.width / 12) * 100}%`
+            : "fit-content" || "",
+        height: this.size == "absolute" ? this.height : "fit-content" || "",
+      };
+    },
+  },
+};
+</script>

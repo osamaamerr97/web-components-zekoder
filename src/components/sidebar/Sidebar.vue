5ce9cbe76fe064a6d777fc7caed4cba94b2c76cd
AbdAsh <59181667+AbdAsh@users.noreply.github.com>
2022-02-22 04:34:34 +0300

    Side Implemented

--- src/components/sidebar/Sidebar.vue
+++ src/components/sidebar/Sidebar.vue
@@ -1,5 +1,31 @@
 <template>
   <div :style="styleObject">
+    <li class="link-container">
+      <a
+        :title="collapsed ? 'Collapse' : 'Expand'"
+        class="link"
+        @click="onCollapse"
+      >
+        <i class="icon fa fa-bars" :style="{ width: collapsedWidth }"></i>
+      </a>
+    </li>
+    <li v-for="(link, i) in links" :key="i" class="link-container">
+      <a
+        :href="link.url"
+        :title="link.tooltip"
+        class="link"
+        :style="link.isActive ? { color: activeColor } : ''"
+      >
+        <i
+          v-if="link.icon"
+          class="icon"
+          :class="['fa', 'fa-' + link.icon]"
+          :style="{ width: collapsedWidth }"
+        ></i>
+        <span v-if="link.label && !collapsed">{{ link.label }}</span>
+      </a>
+    </li>
+    <i />
   </div>
 </template>
 
@@ -40,11 +66,16 @@ export default {
     styleObject() {
       return {
         ...this.styleObj,
+        width: this.collapsed
+          ? this.collapsedWidth
+          : this.width || this.styleObj.width || "",
+        backgroundColor:
+          this.backgroundColor || this.styleObj.backgroundColor || "",
       };
     },
   },
   methods: {
-    toggleCollapsed(event) {
+    onCollapse(event) {
       this.collapsed = !this.collapsed;
       this.$emit("onCollapse", event);
     },
@@ -52,5 +83,36 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.link-container {
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  flex-direction: column;
+  cursor: pointer;
+  list-style: none;
+}
+.link {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  cursor: pointer;
+  color: #fff;
+  font-size: 1.2rem;
+  font-weight: bold;
+  text-decoration: none;
+  transition: all 0.2s ease-in-out;
+  width: 100%;
+  height: 100%;
+}
+.link span {
+  margin-left: 0.5rem;
+}
+.link:hover {
+  color: var(--active-color);
+}
+.link .icon {
+  padding: 0.5rem;
+  text-align: center;
+}
 </style>

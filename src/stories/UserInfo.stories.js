7f5004d0ce618b627e82859c56552955647536b2
AbdAsh <59181667+AbdAsh@users.noreply.github.com>
2022-03-01 12:35:42 +0300

    Added extra styling for the dropdown component

--- src/stories/UserInfo.stories.js
+++ src/stories/UserInfo.stories.js
@@ -50,6 +50,11 @@ export default {
                 type: 'object'
             }
         },
+        dropdownItemStyleObj: {
+            control: {
+                type: 'object'
+            }
+        },
     }
 };
 

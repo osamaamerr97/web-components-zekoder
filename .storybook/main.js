3728d433304055c63eea7eaa8f3d682c97ad3b53
a.mahmutoglu <a.mahmutoglu@cyberneticlabs.io>
2022-10-31 18:59:14 +0300

    Not Clean Fix, will be further cleaned

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,37 +1,32 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)",
-    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-postcss",
-    "@storybook/addon-essentials",
-    "@socheatsok78/storybook-addon-vuetify"
-  ],
-  "framework": "@storybook/vue",
-  core: {
-    builder: 'webpack5',
-  },
-  webpackFinal: async (config, { configType }) => {
-    const rules = config.module.rules.filter((rule) => RegExp(rule.test).toString() !== "/\\.css$/");
-    rules.push(
-      {
-        test: /\.(sc|c)ss$/,
-        use: [
-          'vue-style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
+    "stories": [
+        "../src/**/*.stories.mdx",
+        "../src/**/*.stories.@(js|jsx|ts|tsx)",
+        "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"
+    ],
+    "addons": [
+        "@storybook/addon-links",
+        "@storybook/addon-postcss",
+        "@storybook/addon-essentials",
+        "@socheatsok78/storybook-addon-vuetify"
+    ],
+    "framework": "@storybook/vue",
+    core: {
+        builder: 'webpack5',
+    },
+    webpackFinal: async (config, { configType }) => {
+        config.module.rules.find(rule => RegExp(rule.test).toString() === '/\\.css$/').use.find(rule => rule.loader.includes('\\css-loader') ? rule.options = { modules: true } : null);
+        config.module.rules.push(
+            {
+                test: /\.scss$/,
+                sideEffects: true,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
             }
-          },
-          'sass-loader'
-        ]
-      }
-    )
-    config.module.rules = rules;
-    return config;
-  },
+        )
+        return config;
+    },
 }

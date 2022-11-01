1dca584127875e56c016540f649ad42d5b32d093
a.mahmutoglu <a.mahmutoglu@cyberneticlabs.io>
2022-11-01 13:26:58 +0300

    Updated Story accordinlgy

--- src/stories/Sidebar.stories.js
+++ src/stories/Sidebar.stories.js
@@ -21,7 +21,7 @@ export default {
                 type: 'text',
             },
         },
-        links: {
+        sections: {
             control: {
                 type: 'object',
             }
@@ -30,6 +30,12 @@ export default {
             control: {
                 type: 'color',
             },
+        },
+        alignItems: {
+            control: {
+                type: 'select',
+                options: ['start', 'center']
+            }
         },
         styleObj: {
             control: {
@@ -48,37 +54,85 @@ const Template = (args, { argTypes }) => ({
 
 export const Primary = Template.bind({});
 Primary.args = {
-    backgroundColor: '#fff',
-    width: '300px',
+    backgroundColor: '#e4e4e4',
+    width: '3000px',
     collapsedWidth: '60px',
-    links: [
+    allowExpandCollapse: true,
+    sections: [
         {
-            label: 'Home',
-            link: '#',
-            tooltip: 'Home',
-            icon: 'home',
-            isActive: true,
+            title: {
+                label: 'Dashboard',
+                icon: 'fa fa-home',
+                url: '#',
+                tooltip: 'Home',
+                icon: 'fa fa-home',
+                isActive: true,
+                isHovering: false,
+                showArrow: true,
+            },
+            links: [
+                {
+                    label: 'Home',
+                    url: '#',
+                    tooltip: 'Home',
+                    icon: 'fa fa-home',
+                    isActive: false,
+                    isHovering: false,
+                },
+                {
+                    label: 'About',
+                    url: '#',
+                    tooltip: 'About',
+                    icon: 'fa fa-info',
+                    isActive: false,
+                    isHovering: false,
+                },
+                {
+                    label: 'Contact',
+                    url: '#',
+                    tooltip: 'Contact',
+                    icon: 'fa fa-phone',
+                    isActive: false,
+                    isHovering: false,
+                },
+            ]
         },
         {
-            label: 'About',
-            link: '#',
-            tooltip: 'About',
-            icon: 'info',
-            isActive: false,
-        },
-        {
-            label: 'Contact',
-            link: '#',
-            tooltip: 'Contact',
-            icon: 'phone',
-            isActive: false,
-        },
+            title: {
+                label: 'Utils',
+                url: '#',
+                tooltip: 'Settings',
+                icon: 'fa fa-cog',
+                isActive: false,
+                isHovering: false,
+                showArrow: false,
+            },
+            links: [
+                {
+                    label: 'Settings',
+                    url: '#',
+                    tooltip: 'Settings',
+                    icon: 'fa fa-cog',
+                    isActive: false,
+                    isHovering: false,
+                },
+                {
+                    label: 'Logout',
+                    url: '#',
+                    tooltip: 'Logout',
+                    icon: 'fa fa-arrow-left',
+                    isActive: false,
+                    isHovering: false,
+                },
+            ],
+        }
     ],
     activeColor: '#000',
     styleObj: {
         "color": "red",
         "fontSize": "24px",
         "textTransform": "capitalize",
+        "height": "3000px"
     }
 
 };

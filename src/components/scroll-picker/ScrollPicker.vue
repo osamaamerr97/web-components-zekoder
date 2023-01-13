71462b4d5140c7db3e4944b84b85e0abdf452c15
a.mahmutoglu <a.mahmutoglu@cyberneticlabs.io>
2023-01-13 17:17:40 +0300

    Scroll Picker Fix

---
+++ src/components/scroll-picker/ScrollPicker.vue
@@ -0,0 +1,233 @@
+<template>
+    <div style="display:flex" :styleObject="styleObject">
+        <ScrollPicker
+            v-if="type.includes('date')"
+            :options="years"
+            v-model="value.year"
+            @input="onChange"
+            :class="`zek-scroll-picker ${customClass} years`"
+        />
+        <ScrollPicker
+            v-if="type.includes('date')"
+            style="pointer-events:none; width: 5px"
+            :placeholder="dateSplitter"
+        />
+        <ScrollPicker
+            v-if="type.includes('date')"
+            :options="months"
+            v-model="value.month"
+            @input="onChange"
+            :class="`zek-scroll-picker ${customClass} months`"
+        />
+        <ScrollPicker
+            v-if="type.includes('date')"
+            style="pointer-events:none; width: 5px"
+            :placeholder="dateSplitter"
+        />
+        <ScrollPicker
+            v-if="type.includes('date')"
+            :options="days"
+            v-model="value.day"
+            @input="onChange"
+            :class="`zek-scroll-picker ${customClass} days`"
+        />
+        <ScrollPicker
+            v-if="type.includes('time')"
+            :options="hours"
+            v-model="value.hour"
+            @input="onChange"
+            :class="`zek-scroll-picker ${customClass} hours`"
+        />
+        <ScrollPicker style="pointer-events:none; width: 5px" :placeholder="timeSplitter" />
+        <ScrollPicker
+            v-if="type.includes('time')"
+            :options="minutes"
+            v-model="value.minutes"
+            @input="onChange"
+            :class="`zek-scroll-picker ${customClass} minutes`"
+        />
+        <ScrollPicker
+            v-if="type.includes('time') && !H24"
+            :options="[
+                { name: 'AM', value: true },
+                { name: 'PM', value: false }
+            ]"
+            @input="onChange"
+            :class="`zek-scroll-picker ${customClass} am_pm`"
+        />
+        <ScrollPicker v-if="type == 'custom'" v-bind="custom" />
+    </div>
+</template>
+
+<script>
+import { ScrollPicker } from "vue-scroll-picker";
+
+export default {
+    name: "ZekScrollPicker",
+    components: {
+        ScrollPicker
+    },
+    props: {
+        activeColor: {
+            type: String,
+            default: "blue"
+        },
+        type: {
+            type: String, // date, time, datetime, custom
+            default: "datatime"
+        },
+        customClass: {
+            type: String,
+            default: ""
+        },
+        monthFormat: {
+            type: String, // short, long, number
+            default: "short"
+        },
+        minDate: {
+            type: String,
+            required: false,
+            default: "2022-02-01 00:00"
+        },
+        maxDate: {
+            type: String,
+            required: false,
+            default: "2100-01-01 00:00"
+        },
+        height: {
+            type: String,
+            default: "1em"
+        },
+        width: {
+            type: String,
+            default: "1em"
+        },
+        initialValue: {
+            type: String,
+            default: ""
+        },
+        styleObject: {
+            type: Object,
+            required: false,
+            default: () => ({})
+        },
+        H24: {
+            type: Boolean,
+            default: false
+        },
+        custom: {
+            type: Object,
+            required: false
+        },
+        dateSplitter: {
+            type: String,
+            default: "-"
+        },
+        timeSplitter: {
+            type: String,
+            default: ":"
+        },
+        margin: {
+            type: String,
+        },
+        fontSize: {
+            type: String,
+        }
+    },
+    data() {
+        return {
+            value: {
+                year: new Date().getFullYear(),
+                month: new Date().getMonth() + 1,
+                day: new Date().getDate(),
+                hour: this.H24 ? new Date().getHours() : new Date().getHours() % 12,
+                minutes: new Date().getMinutes()
+            },
+            minutes: new Array(60).fill(0).map((_, i) => {
+                return { name: i + 1 < 10 ? `0${i + 1}` : `${i + 1}`, value: i + 1 };
+            })
+        };
+    },
+    created() {
+        if (this.initialValue) {
+            const date = new Date(this.initialValue);
+            this.value.year = `${date.getFullYear()}`;
+            this.value.month = `${date.getMonth() + 1}`;
+            this.value.day = `${date.getDate()}`;
+            this.value.hour = `${date.getHours()}`;
+            this.value.minutes = `${date.getMinutes()}`;
+        }
+    },
+    methods: {
+        onChange() {
+            const { year, month, day, hour, minutes } = this.value;
+            this.$emit("onChange", {
+                dateString: `${year}-${month}-${day} ${hour}:${minutes} ${this.H24 ? "AM" : "PM"}`,
+                time: `${hour}:${minutes} ${this.H24 ? "AM" : "PM"}`,
+                date: new Date(year, month, day, hour, minutes),
+                value: this.value
+            });
+        }
+    },
+    computed: {
+        years() {
+            const min = new Date(this.minDate);
+            const max = new Date(this.maxDate);
+            const years = [];
+            for (let i = min.getFullYear(); i <= max.getFullYear(); i++) {
+                years.push(i);
+            }
+            return years;
+        },
+        months() {
+            const months = new Array(12).fill(0).map((_, i) => i);
+            if (this.monthFormat === "short") {
+                return months.map(month => {
+                    const date = new Date(2021, month, 1);
+                    return { name: date.toLocaleString("default", { month: "short" }), value: month + 1 };
+                });
+            } else if (this.monthFormat === "long") {
+                return months.map(month => {
+                    const date = new Date(2021, month, 1);
+                    return { name: date.toLocaleString("default", { month: "short" }), value: month + 1 };
+                });
+            } else return new Array(12).fill(0).map((_, i) => i + 1);
+        },
+        days() {
+            const { year, month } = this.value;
+            const days = new Date(year, month, 0).getDate();
+            return new Array(days).fill(0).map((_, i) => i + 1);
+        },
+        hours() {
+            if (this.H24) {
+                return new Array(24).fill(0).map((_, i) => {
+                    return { name: i + 1 < 10 ? `0${i + 1}` : `${i + 1}`, value: i + 1 };
+                });
+            } else {
+                return new Array(24).fill(0).map((_, i) => {
+                    return { name: i + 1 < 10 ? `0${i + 1}` : `${i + 1}`, value: i + 1 };
+                });
+            }
+        }
+    }
+};
+</script>
+
+<style scoped>
+@import "./style.css";
+.vue-scroll-picker {
+    max-height: v-bind(height);
+    width: v-bind(width);
+}
+:deep .top {
+    border: none;
+}
+:deep .bottom {
+    border: none;
+}
+:deep .-selected {
+    color: v-bind(activeColor);
+    margin: v-bind(selectedMargin);
+    font-size: v-bind(selectedFontSize);
+}
+</style>
---
+++ src/stories/ScrollPicker.stories.js
@@ -0,0 +1,43 @@
+import ZekScrollPicker from '../components/scroll-picker/ScrollPicker.vue';
+import { action } from "@storybook/addon-actions"
+export default {
+    title: 'Zekoder/Scroll Picker',
+    component: ZekScrollPicker,
+    argTypes: {
+        type: {
+            control: {
+                type: 'select',
+                options: ['date', 'time', 'datetime']
+            }
+        },
+        monthFormat: {
+            control: {
+                type: 'select',
+                options: ['long', 'short', 'number']
+            }
+        },
+    }
+};
+
+const Template = (args, { argTypes }) => ({
+    components: { ZekScrollPicker },
+    props: Object.keys(argTypes),
+    methods: { action },
+    template: '<ZekScrollPicker v-bind="$props" @onChange="action(`onChange`)($event)" />',
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+    type: 'datetime',
+    // initialValue: '1997-01-01 00:00',
+    monthFormat: 'long',
+    minDate: '1990-01-01',
+    maxDate: '2020-12-31',
+    width: '300px',
+    height: '300px',
+    selectedMargin: '0 0 0 10px',
+    selectedFontSize: '20px',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {};

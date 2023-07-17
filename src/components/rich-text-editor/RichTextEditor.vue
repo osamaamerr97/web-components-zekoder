c7dd9c8cd1215be459c89a07b220e92162736574
a.mahmutoglu <a.mahmutoglu@cyberneticlabs.io>
2023-07-17 17:40:29 +0300

    Rich Text Editor Modification

--- src/components/rich-text-editor/RichTextEditor.vue
+++ src/components/rich-text-editor/RichTextEditor.vue
@@ -1,60 +1,72 @@
 <template>
-  <vue-editor
-    v-model="value"
-    :placeholder="placeholder"
-    :disabled="disabled"
-    :editorToolbar="toolbar"
-    @text-change="textChange"
-    v-bind="extraProps"
-    v-on="extraEvents"
-  />
+  <yimo-vue-editor :class="`zek-rich-editor` + customClass" v-model="value" :config="config" @input="textChange"></yimo-vue-editor>
 </template>
 
 <script>
-import { VueEditor } from "vue2-editor";
+import YimoVueEditor, {E} from "yimo-vue-editor";
 export default {
   name: "ZekRichTextEditor",
   components: {
-    VueEditor,
+    YimoVueEditor
   },
   props: {
+    customClass: {
+      type: String,
+      required: false
+    },
     initialValue: {
       type: String,
-      required: false,
+      required: false
     },
     placeholder: {
       type: String,
-      required: false,
+      required: false
     },
     disabled: {
       type: Boolean,
-      required: false,
+      required: false
     },
-    toolbar: { // Option can be found here: https://github.com/davidroyer/vue2-editor/blob/master/src/helpers/fullToolbar.js
+    toolbar: {
+      // Option can be found here: https://github.com/davidroyer/vue2-editor/blob/master/src/helpers/fullToolbar.js
       type: Array,
-      required: false,
+      required: false
     },
-    extraProps: {
+    options: {
       type: Object,
       required: false,
-    },
-    extraEvents: {
-      type: Object,
-      required: false,
-    },
+      default: () => ({})
+    }
   },
   data() {
     return {
       value: this.initialValue ?? "",
+      config: {
+        printLog: false, // disabled console.log
+        lang: E.langs.en,// lang config
+        menus: this.toolbar,
+        ...this.options, // node_modules/yimo-vue-editor/src/assets/js/wangEditor.js 
+      }
     };
+  },
+  created() {
+    E.placeholder = this.placeholder ?? "";
+    if (this.disabled) {
+      this.config.menus = [];
+      // remove pointer events from zek-rich-editor class
+      this.customClass = this.customClass + " disabled";
+    }
   },
   methods: {
     textChange(e) {
-      this.$emit("onChange", this.value);
+      // if disabled don't change value
+      this.$emit("onChange", e);
     },
   }
 };
 </script>
 
-<style>
+<style scoped lang="scss">
+.disabled {
+  pointer-events: none;
+}
 </style>

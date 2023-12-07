03cfdeac1c5937459b4ace34c984d10c24c37c9c
a.mahmutoglu <a.mahmutoglu@cyberneticlabs.io>
2023-12-07 12:25:26 +0300

    Fix up Column Content Component, remove redundency + fix up imports

--- src/components/column-content/ColumnContent.vue
+++ src/components/column-content/ColumnContent.vue
@@ -1,341 +1,115 @@
 <template>
-    <div class="column-content-wrapper"
-        v-if="column && ((column.rows && column.rows.length) || (column.content))"
+    <div
+        class="column-content-wrapper"
+        v-if="column && ((column.rows && column.rows.length) || column.content)"
         v-show="column.condition != undefined ? column.condition : true"
         :key="key"
         :class="{
             customClass,
-            'content-container': column.content && Array.isArray(column.content),
+            'content-container': column.content && Array.isArray(column.content)
         }"
         :style="styleObj"
     >
         <template v-if="column && column.rows && column.rows.length">
-            <div v-for="(row,i) in column.rows" :key="'row'+i" class="row"
+            <div
+                v-for="(row, i) in column.rows"
+                :key="'row' + i"
+                class="row"
                 v-bind="row.props"
                 v-on="row.events"
                 :id="row.id"
                 :class="row.class"
                 v-show="row.condition != undefined ? row.condition : true"
-                @click.stop="emitClick('rowClicked', {column: column, row: row, index: i})"
+                @click.stop="emitClick('rowClicked', { column: column, row: row, index: i })"
             >
-                <div v-for="(col,index) in row.columns" :key="'col'+index"
-                    :class="(col.columnWidth ? 'col-'+col.columnWidth : 'col')+' '+(col.class || '')"
+                <div
+                    v-for="(col, index) in row.columns"
+                    :key="'col' + index"
+                    :class="(col.columnWidth ? 'col-' + col.columnWidth : 'col') + ' ' + (col.class || '')"
                     :id="col.id"
                     v-bind="col.props"
                     v-on="col.events"
                     v-show="col.condition != undefined ? col.condition : true"
-                    @click.stop="emitClick('colClicked', {column: col, row: row, index: index})"
+                    @click.stop="emitClick('colClicked', { column: col, row: row, index: index })"
                 >
-                    <zek-column-content :column="col" @rowClicked="emitClick('rowClicked', $event)" @colClicked="emitClick('colClicked', $event)"></zek-column-content>
+                    <zek-column-content
+                        :column="col"
+                        @rowClicked="emitClick('rowClicked', $event)"
+                        @colClicked="emitClick('colClicked', $event)"
+                    ></zek-column-content>
                 </div>
             </div>
         </template>
         <template v-else-if="column && column.content && Array.isArray(column.content) && column.content.length">
-            <zek-column-content v-for="(content,i) in column.content" :key="'content'+i+key" :column="{content, condition: content.condition, map: content.map}"></zek-column-content>
+            <zek-column-content
+                v-for="(content, i) in column.content"
+                :key="'content' + i + key"
+                :column="{ content, condition: content.condition }"
+            ></zek-column-content>
         </template>
-        <zek-button
-            @click="stopPropagation($event)"
-            v-else-if="column && column.content && column.content.component == 'button'"
-            v-bind="column.content.data"
-            v-on="column.content.events"
-        ></zek-button>
-        <zek-heading
-            v-else-if="column && column.content && column.content.component == 'heading'"
-            v-bind="column.content.data"
-            v-on="column.content.events"
-        ></zek-heading>
-        <zek-html
-            v-else-if="column && column.content && column.content.component == 'html'"
-            v-bind="column.content.data"
-            v-on="column.content.events"
-        ></zek-html>
-        <zek-image
-            v-else-if="column && column.content && column.content.component == 'image'"
-            v-bind="column.content.data"
-            v-on="column.content.events"
-        ></zek-image>
-        <i
-            v-else-if="column && column.content && column.content.component == 'icon'"
-            class="card-icon"
-            :class="column.content.icon"
-        ></i>
-        <zek-list
-            v-else-if="column && column.content && column.content.component == 'list'"
-            v-bind="column.content.data"
-            v-on="column.content.events"
-        ></zek-list>
-        <zek-text
-            v-else-if="column && column.content && column.content.component == 'text'"
-            class="card-text"
-            v-bind="column.content.data"
-            v-on="column.content.events"
-            :dataField="apiData ? apiData[column.content.dataField] : ''"
-        ></zek-text>
-        <zek-initials
-            v-else-if="column && column.content && column.content.component == 'initials'"
-            class="card-initials"
-            v-bind="column.content.data"
-        ></zek-initials>
-        <client-only v-else-if="column && column.content && column.content.component == 'video'">
-            <zek-video v-bind="column.content.data" v-on="column.content.events"></zek-video>
-        </client-only>
-        <zek-radio-button
-            @click="stopPropagation($event)"
-            v-else-if="column && column.content && column.content.component == 'radio-button'"
-            v-bind="column.content.data"
-            v-on="column.content.events"
-        ></zek-radio-button>
-        <zek-dropdown
-            @click="stopPropagation($event)"
-            v-else-if="column && column.content && column.content.component == 'dropdown'"
-            v-bind="column.content.data"
-            v-on="column.content.events"
-        ></zek-dropdown>
-        <zek-countries-list
-            @click="stopPropagation($event)"
-            v-else-if="column && column.content && column.content.component == 'countries-list'"
-            v-bind="column.content.data"
-            v-on="column.content.events"
-        ></zek-countries-list>
-        <zek-input
-            @click="stopPropagation($event)"
-            v-else-if="column && column.content && column.content.component == 'input'"
-            v-bind="column.content.data"
-            v-on="column.content.events"
-        ></zek-input>
-        <zek-form
-            @click="stopPropagation($event)"
-            v-else-if="column && column.content && column.content.component == 'form'"
-            v-bind="column.content.data"
-            v-on="column.content.events"
-        ></zek-form>
-        <zek-textarea
-            @click="stopPropagation($event)"
-            v-else-if="column && column.content && column.content.component == 'textarea'"
-            v-bind="column.content.data"
-            v-on="column.content.events"
-        ></zek-textarea>
-        <zek-card
-            v-else-if="column && column.content && column.content.component == 'card'"
-            v-bind="column.content.data"
-            v-on="column.content.events"
-        ></zek-card>
-        <zek-collapsible-container
-            v-else-if="column && column.content && column.content.component == 'collapsible-container'"
-            v-bind="column.content.data"
-            v-on="column.content.events"
-        ></zek-collapsible-container>
-        <zek-table
-            v-else-if="column && column.content && column.content.component == 'table'"
-            v-bind="column.content.data"
-            v-on="column.content.events"
-        ></zek-table>
-        <zek-file-upload
-            v-else-if="column && column.content && column.content.component == 'file-upload'"
-            v-bind="column.content.data"
-            v-on="column.content.events"
-        ></zek-file-upload>
-        <zek-toggle-button
-            v-else-if="column && column.content && column.content.component == 'toggle-button'"
-            v-bind="column.content.data"
-            v-on="column.content.events"
-        ></zek-toggle-button>
-        <zek-slider
-            v-else-if="column && column.content && column.content.component == 'slider'"
-            v-bind="column.content.data"
-            v-on="column.content.events"
-        ></zek-slider>
-        <component
-            v-else-if="column && column.content && column.content.type == 'custom'"
-            :is="column.content.component"
-            v-bind="column.content.data"
-            v-on="column.content.events"
-        ></component>
-        <div
-            v-else-if="column && column.content && column.content.component == 'captcha'"
-            v-bind="column.content.data"
-            v-on="column.content.events"
-        >
-            <vue-recaptcha
-                ref="recaptcha"
-                @verify="column.content.events.onVerify"
-                @expired="column.content.events.onExpired"
-                :loadRecaptchaScript="true"
-                :sitekey="column.content.data.siteKey"
-            ></vue-recaptcha>
-        </div>
+        <template v-else-if="column && column.content && column.content.type == 'custom'">
+            <component
+                :is="column.content.component"
+                v-bind="column.content.data"
+                v-on="column.content.events"
+            ></component>
+        </template>
+        <template v-else-if="column && column.content">
+            <component
+                :is="components[column.content.component]"
+                v-bind="column.content.data"
+                v-on="column.content.events"
+            ></component>
+        </template>
     </div>
 </template>
-
 <script>
-import ZekButton from "../action-button/ActionButton.vue";
-import ZekInitials from "../initials/Initials.vue";
-import ZekHeading from "../heading-block/HeadingBlock.vue";
-import ZekHtml from "../html-block/HtmlBlock.vue";
-import ZekImage from "../image/Image.vue";
-import ZekList from "../lists-block/ListsBlock.vue";
-import ZekText from "../text-block/TextBlock.vue";
-import ZekVideo from "../video/Video.vue";
-import ZekRadioButton from "../radio-button/RadioButton.vue";
-import ZekDropdown from "../dropdown/Dropdown.vue";
-import ZekInput from "../input-field/InputField.vue";
-import ZekForm from "../form/Form.vue";
-import ZekTextarea from "../textarea/Textarea.vue";
-import ZekCard from "../card/Card.vue";
-import ZekCollapsibleContainer from "../collapsible-container/CollapsibleContainer.vue";
-import ZekTable from "../table/Table.vue";
-import ZekToggleButton from "../toggle-button/ToggleButton.vue";
-import VueRecaptcha from "../../../node_modules/vue-recaptcha/dist/vue-recaptcha.es";
-import ZekCountriesList from "../countries-list/CountriesList.vue";
-import axios from "axios";
-import ZekFileUpload from '../file-upload/FileUpload.vue';
-import ZekSlider from '../slider/Slider.vue';
-
 export default {
-    components: {
-        ZekButton,
-        ZekHeading,
-        ZekHtml,
-        ZekImage,
-        ZekList,
-        ZekText,
-        ZekInitials,
-        ZekVideo,
-        ZekRadioButton,
-        ZekDropdown,
-        ZekInput,
-        ZekTextarea,
-        ZekCard,
-        ZekCollapsibleContainer,
-        ZekTable,
-        ZekToggleButton,
-        ZekCountriesList,
-        VueRecaptcha,
-        ZekFileUpload,
-        ZekSlider,
-        ZekForm
-    },
     name: "ZekColumnContent",
     props: {
-        column: Object, //column can have rows or a component. Each row must have columns, columns can have more rows. Component can only be inside a column
+        column: Object,
         customClass: {
             type: String,
             default: ""
         },
         styleObj: {
             type: Object,
-            default: () => { return {} }
-        }
-
-    },
-    data(){
-        return {
-            key: Math.ceil(Math.random() * 100000),
-            apiData: null
-        }
-    },
-    computed: {
-        isColumnArray() {
-            return Array.isArray(this.column.content) && this.column.content.length ? true : false;
-        },
-        isRows() {
-            return this.column && this.column.rows && !this.column.dataSource ? true : false;
-        },
-        isParent() {
-            return this.column.dataSource && this.column.rows ? true : false;
+            default: () => {
+                return {};
+            }
         }
     },
     created() {
-        this.init()
+        console.log("column", this.components);
+    },
+    data() {
+        return {
+            key: Math.ceil(Math.random() * 100000),
+            components: {
+                button: require("../action-button/ActionButton.vue").default,
+                initials: require("../initials/Initials.vue").default,
+                heading: require("../heading-block/HeadingBlock.vue").default,
+                html: require("../html-block/HtmlBlock.vue").default,
+                image: require("../image/Image.vue").default,
+                list: require("../lists-block/ListsBlock.vue").default,
+                text: require("../text-block/TextBlock.vue").default,
+                video: require("../video/Video.vue").default,
+                "radio-button": require("../radio-button/RadioButton.vue").default,
+                dropdown: require("../dropdown/Dropdown.vue").default,
+                input: require("../input-field/InputField.vue").default,
+                form: require("../form/Form.vue").default,
+                textarea: require("../textarea/Textarea.vue").default,
+                card: require("../card/Card.vue").default,
+                "collapsible container": require("../collapsible-container/CollapsibleContainer.vue").default,
+                table: require("../table/Table.vue").default,
+                "toggle-button": require("../toggle-button/ToggleButton.vue").default,
+                "file-upload": require("../file-upload/FileUpload.vue").default,
+                slider: require("../slider/Slider.vue").default,
+                captcha: require("../re-captcha/ReCaptcha.vue").default
+            }
+        };
     },
     methods: {
-        init() {
-            if(this.column.map) {
-                this.processMap(this.column.map);
-            } else if (this.column && this.column.dataSource) {
-                this.processDataSource(this.column.dataSource);
-            } else if (this.column && this.column.rows) {
-                this.column.rows.forEach(row => {
-                    if (row.dataSource) {
-                        this.processDataSource(row.dataSource);
-                    }
-                });
-            }
-        },
-        processMap(map) {
-            // TODO: This could be a lot cleaner
-            if(this.isColumnArray){
-                console.log("Column Array")
-                if(this.column.dataSource && this.column.dataSource.iter){
-                    let colGroup = this.column.content;
-                    this.column.content = [];
-                    map.forEach(m => {
-                        this.column.content = this.column.content.concat(this.mapColGrouptoMap(colGroup, m))
-                    });
-                } else {
-                    this.column.content.forEach((column, c) => {
-                        column.map = map[c]
-                    });
-                }
-            } else if(this.isRows){
-                console.log("Rows")
-                this.column.rows.forEach((row, r) => {
-                    if(row.dataSource && row.dataSource.iter){
-                        let colGroup = row.columns;
-                        row.columns = [];
-                        map.forEach(m => {
-                            row.columns = row.columns.concat(this.mapColGrouptoMap(colGroup, m));
-                        });
-                    } else {
-                        row.columns.forEach(column => {
-                            column.map = map[r]
-                        });
-                    }
-                });
-            } else if (this.isParent) {
-                console.log("Parent")
-                if(this.column.dataSource && this.column.dataSource.iter){
-                    let colGroup = this.column.rows;
-                    this.column.rows = [];
-                    map.forEach(m => {
-                        this.column.rows = this.column.rows.concat(this.mapColGrouptoMap(colGroup, m, true));
-                    });
-                } else {
-                    this.column.rows.forEach((row, r) => {
-                        row.columns.forEach(column => {
-                            column.map = map[r]
-                        });
-                    });
-                }
-            }
-            else {
-                console.log("Column")
-                this.apiData = Array.isArray(map) ? map[0] : map;
-            }
-        },
-        mapColGrouptoMap(colGroup, map, isRow) {
-            return colGroup.map(col => {
-                if (isRow){
-                    col.columns = this.mapColGrouptoMap(col.columns, map)
-                    return {...col, map: map};
-                } else{
-                    return {...col, map: map};
-                }
-            });
-        },
-        processDataSource(dataSource) {
-            axios({
-                method: dataSource.method,
-                url: `${dataSource.url}/q`,
-                data: dataSource.requestBody,
-                headers: dataSource.headers
-            }).then(response => {
-                this.processMap(response.data.data);
-                this.key++;
-            }).catch(error => {
-                console.log(error)
-            });
-        },
         stopPropagation(event) {
             event.stopPropagation();
         },

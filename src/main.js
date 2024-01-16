f738cb8a79b9448e4525cb90fed0dd82cf460416
a.mahmutoglu <a.mahmutoglu@cyberneticlabs.io>
2024-01-16 11:47:58 +0300

    Fixes on config

--- src/main.js
+++ src/main.js
@@ -1,56 +1,48 @@
-import './set-public-path';
-// SECTION: Low-level components
-const ZekButton = () => import(/* webpackChunkName: "zek-button" */ './components/action-button/ActionButton.vue');
-const ZekCard = () => import(/* webpackChunkName: "zek-card" */ './components/card/Card.vue');
-const ZekCardsList = () => import(/* webpackChunkName: "zek-cards-list" */ './components/cards-list/CardsList.vue');
-const ZekColumnContent = () => import(/* webpackChunkName: "zek-column-content" */ './components/column-content/ColumnContent.vue');
-const ZekHeading = () => import(/* webpackChunkName: "zek-heading" */ './components/heading-block/HeadingBlock.vue');
-const ZekHeader = () => import(/* webpackChunkName: "zek-header" */ './components/header/Header.vue');
-const ZekFooter = () => import(/* webpackChunkName: "zek-footer" */ './components/footer/Footer.vue');
-const ZekHtml = () => import(/* webpackChunkName: "zek-html" */ './components/html-block/HtmlBlock.vue');
-const ZekList = () => import(/* webpackChunkName: "zek-list" */ './components/lists-block/ListsBlock.vue');
-const ZekSlider = () => import(/* webpackChunkName: "zek-slider" */ './components/slider/Slider.vue');
-const ZekText = () => import(/* webpackChunkName: "zek-text" */ './components/text-block/TextBlock.vue');
-const ZekSidebar = () => import(/* webpackChunkName: "zek-sidebar" */ './components/sidebar/Sidebar.vue');
-const ZekInitials = () => import(/* webpackChunkName: "zek-initials" */ './components/initials/Initials.vue');
-const ZekCollapsibleContainer = () => import(/* webpackChunkName: "zek-collapsible-container" */ './components/collapsible-container/CollapsibleContainer.vue');
-const ZekRadioButton = () => import(/* webpackChunkName: "zek-radio-button" */ './components/radio-button/RadioButton.vue');
-const ZekDropdown = () => import(/* webpackChunkName: "zek-dropdown" */ './components/dropdown/Dropdown.vue');
-const ZekInput = () => import(/* webpackChunkName: "zek-input" */ './components/input-field/InputField.vue');
-const ZekTextarea = () => import(/* webpackChunkName: "zek-textarea" */ './components/textarea/Textarea.vue');
-const ZekTable = () => import(/* webpackChunkName: "zek-table" */ './components/table/Table.vue');
-const ZekRightPanel = () => import(/* webpackChunkName: "zek-right-panel" */ './components/right-panel/RightPanel.vue');
-const ZekPopup = () => import(/* webpackChunkName: "zek-popup" */ './components/popup/Popup.vue');
-const ZekLoader = () => import(/* webpackChunkName: "zek-loader" */ './components/loader/Loader.vue');
-// ? Auth components
-const ZekLogin = () => import(/* webpackChunkName: "zek-login" */ './components/login/Login.vue');
-const ZekForm = () => import(/* webpackChunkName: "zek-form" */ './components/form/Form.vue');
-const ZekSignup = () => import(/* webpackChunkName: "zek-signup" */ './components/signup/Signup.vue');
-const ZekGuidedForm = () => import(/* webpackChunkName: "zek-guided-form" */ './components/guided-form/GuidedForm.vue');
-//? Media components
-const ZekImage = () => import(/* webpackChunkName: "zek-image" */ './components/image/Image.vue');
-const ZekUpload = () => import(/* webpackChunkName: "zek-upload" */ './components/upload-component/Upload.vue');
-const ZekVideo = () => import(/* webpackChunkName: "zek-video" */ './components/video/Video.vue');
-const ZekFileUpload = () => import(/* webpackChunkName: "zek-file-upload" */ './components/file-upload/FileUpload.vue');
-
-// SECTION: High-level components
-const ZekScrollPicker = () => import(/* webpackChunkName: "zek-scroll-picker" */ './components/scroll-picker/ScrollPicker.vue');
-const ZekDocViewer = () => import(/* webpackChunkName: "zek-doc-viewer" */ './components/doc-viewer/DocViewer.vue');
-const ZekUserInfo = () => import(/* webpackChunkName: "zek-user-info" */ './components/user-info-component/UserInfo.vue');
-const ZekCountriesList = () => import(/* webpackChunkName: "zek-countries-list" */ './components/countries-list/CountriesList.vue');
-const ZekImageGallery = () => import(/* webpackChunkName: "zek-image-gallery" */ './components/image-gallery/ImageGallery.vue');
-const ZekScheduler = () => import(/* webpackChunkName: "zek-scheduler" */ './components/scheduler/Scheduler.vue');
-const ZekProgressBar = () => import(/* webpackChunkName: "zek-progress-bar" */ './components/progress-bar/ProgressBar.vue');
-const ZekCalendar = () => import(/* webpackChunkName: "zek-calendar" */ './components/calendar/Calendar.vue');
-// ? Editors
-const ZekCodeEditor = () => import(/* webpackChunkName: "zek-code-editor" */ './components/code-editor/CodeEditor.vue');
-const ZekRichTextEditor = () => import(/* webpackChunkName: "zek-rich-text-editor" */ './components/rich-text-editor/RichTextEditor.vue');
-// ? Charts
-const ZekXYChart = () => import(/* webpackChunkName: "zek-xy-chart" */ './components/charts/XYChart.vue');
-const ZekPieChart = () => import(/* webpackChunkName: "zek-pie-chart" */ './components/charts/PieChart.vue');
-const ZekRadialChart = () => import(/* webpackChunkName: "zek-radial-chart" */ './components/charts/RadialChart.vue');
-const ZekGanttChart = () => import(/* webpackChunkName: "zek-gantt-chart" */ './components/charts/GanttChart.vue');
-const ZekNavbar = () => import(/* webpackChunkName: "zek-navbar" */ './components/navbar/Navbar.vue');
+import ZekButton from './components/action-button/ActionButton.vue';
+import ZekCard from './components/card/Card.vue';
+import ZekCardsList from './components/cards-list/CardsList.vue';
+import ZekColumnContent from './components/column-content/ColumnContent.vue';
+import ZekHeading from './components/heading-block/HeadingBlock.vue';
+import ZekHeader from './components/header/Header.vue';
+import ZekFooter from './components/footer/Footer.vue';
+import ZekHtml from './components/html-block/HtmlBlock.vue';
+import ZekList from './components/lists-block/ListsBlock.vue';
+import ZekSlider from './components/slider/Slider.vue';
+import ZekText from './components/text-block/TextBlock.vue';
+import ZekSidebar from './components/sidebar/Sidebar.vue';
+import ZekInitials from './components/initials/Initials.vue';
+import ZekCollapsibleContainer from './components/collapsible-container/CollapsibleContainer.vue';
+import ZekRadioButton from './components/radio-button/RadioButton.vue';
+import ZekDropdown from './components/dropdown/Dropdown.vue';
+import ZekInput from './components/input-field/InputField.vue';
+import ZekTextarea from './components/textarea/Textarea.vue';
+import ZekTable from './components/table/Table.vue';
+import ZekRightPanel from './components/right-panel/RightPanel.vue';
+import ZekPopup from './components/popup/Popup.vue';
+import ZekLoader from './components/loader/Loader.vue';
+import ZekLogin from './components/login/Login.vue';
+import ZekForm from './components/form/Form.vue';
+import ZekSignup from './components/signup/Signup.vue';
+import ZekGuidedForm from './components/guided-form/GuidedForm.vue';
+import ZekImage from './components/image/Image.vue';
+import ZekUpload from './components/upload-component/Upload.vue';
+import ZekVideo from './components/video/Video.vue';
+import ZekFileUpload from './components/file-upload/FileUpload.vue';
+import ZekScrollPicker from './components/scroll-picker/ScrollPicker.vue';
+import ZekDocViewer from './components/doc-viewer/DocViewer.vue';
+import ZekUserInfo from './components/user-info-component/UserInfo.vue';
+import ZekCountriesList from './components/countries-list/CountriesList.vue';
+import ZekImageGallery from './components/image-gallery/ImageGallery.vue';
+import ZekScheduler from './components/scheduler/Scheduler.vue';
+import ZekProgressBar from './components/progress-bar/ProgressBar.vue';
+import ZekCalendar from './components/calendar/Calendar.vue';
+import ZekCodeEditor from './components/code-editor/CodeEditor.vue';
+import ZekRichTextEditor from './components/rich-text-editor/RichTextEditor.vue';
+import ZekXYChart from './components/charts/XYChart.vue';
+import ZekPieChart from './components/charts/PieChart.vue';
+import ZekRadialChart from './components/charts/RadialChart.vue';
+import ZekGanttChart from './components/charts/GanttChart.vue';
+import ZekNavbar from './components/navbar/Navbar.vue';
 
 export {
     ZekButton,
--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,36 @@ module.exports = {
     css: {
         extract: false
     },
-    lintOnSave: false
+    productionSourceMap: false,
+    lintOnSave: false,
+    configureWebpack: {
+        externals: [/(storybook)/, /(@vue)/, "vue-router"],
+        output: {
+            libraryExport: 'default'
+        },
+        optimization: {
+            splitChunks: {
+                chunks: 'async',
+                minSize: 20000,
+                maxSize: 50000,
+                minChunks: 1,
+                maxAsyncRequests: 30,
+                maxInitialRequests: 30,
+                automaticNameDelimiter: '~',
+                enforceSizeThreshold: 50000,
+                cacheGroups: {
+                    defaultVendors: {
+                        test: /[\\/]node_modules[\\/]/,
+                        priority: -10
+                    },
+                    default: {
+                        minChunks: 2,
+                        priority: -20,
+                        reuseExistingChunk: true
+                    }
+                }
+            },
+            minimize: true
+        }
+    }
 };
